ae9e2e2b1ce5a005b18922145de2ae57dcc98a5a
david perry <perryd11016@gmail.com>
2014-10-03 13:46:12 -0700

    snake just started it

---
+++ public_html/js/snake.js
@@ -0,0 +1,17 @@
+ var snake;
+
+ var context;
+ var screenWidth;
+ var screenHeight;
+ 
+ function gameInitialize() {
+     
+ }
+ 
+ function gameLoop () {
+     
+ }
+ 
+ function gameDraw () {
+     
+ }
