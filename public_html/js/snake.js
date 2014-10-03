06a9f8904e8ea7a8bdc2a92288132f79affc4597
david perry <perryd11016@gmail.com>
2014-10-03 14:04:02 -0700

    still working i got some codes done today and variables

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -5,9 +5,9 @@
  var screenHeight;
  
  function gameInitialize() {
-     
+     var canvas = document.getElementById("game-screen");
+     context = canvas.getContext("2d");
  }
- 
  function gameLoop () {
      
  }
