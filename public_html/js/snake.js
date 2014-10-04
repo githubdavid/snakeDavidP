605fad50521698c9bfa167952b020a672cdb1e3e
david perry <perryd11016@gmail.com>
2014-10-03 23:32:56 -0700

    video 12 i did a few more at home but i was stuck on video 12

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -4,14 +4,22 @@
  var screenWidth;
  var screenHeight;
  
+ gameDraw();
  function gameInitialize() {
      var canvas = document.getElementById("game-screen");
      context = canvas.getContext("2d");
+    
+     screenWidth = window.innerWidth;
+     screenHeight = window.innerHeight;
+     
+     canvas.width = screenWidth;
+     canvas.height = screenHeight;
  }
  function gameLoop () {
      
  }
  
  function gameDraw () {
-     
+     context.fillStyle = "rgb(61, 255, 229)";
+     context.fillRect (0, 0, screenWidth, screenHeght);
  }
