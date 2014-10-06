594c9330252feae4b7f796def1ba4eded7063f6f
david perry <perryd11016@gmail.com>
2014-10-06 14:20:58 -0700

    i did some videos on like 13

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,12 +1,17 @@
  var snake;
+var snakeLength;
+var snakeSize;
 
  var context;
  var screenWidth;
- var screenHeight;
+ var screenHeight;                                                              
  
+gameInitialize();
  gameDraw();
- function gameInitialize() {
-     var canvas = document.getElementById("game-screen");
+ 
+     
+  function gameInitialize() {
+      var canvas = document.getElementById("game-screen");
      context = canvas.getContext("2d");
     
      screenWidth = window.innerWidth;
@@ -15,11 +20,18 @@
      canvas.width = screenWidth;
      canvas.height = screenHeight;
  }
+
+  
+  
  function gameLoop () {
      
  }
  
  function gameDraw () {
-     context.fillStyle = "rgb(61, 255, 229)";
-     context.fillRect (0, 0, screenWidth, screenHeght);
+     context.fillStyle = "rgb(108, 245, 181)";
+     context.fillRect (0, 0, screenWidth, screenHeight);
+ }
+ 
+function snakeInitialize() {
+     
  }
